e1e1dc8a520e7c28d6f300bb1fbd823abaf7b104
canhamzacode <abdulmuizzayo6@gmail.com>
2024-06-23 19:39:02 +0200

    fix: hero animation

--- src/components/TextRotator/TextRotator.tsx
+++ src/components/TextRotator/TextRotator.tsx
@@ -1,19 +1,15 @@
-import { useState, useEffect } from 'react';
 import './TextRotator.css';
-const texts = ['Generative AI', 'Automotive AI', 'Government AI'];
 
 const TextRotator = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, 3000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return <span className="custom-text flip">{texts[currentIndex]}</span>;
+  return (
+    <div className="inner-headings">
+      <span className="custom-text">Generative AI</span>
+      <span className="custom-text">Automotive AI</span>
+      <span className="custom-text">Government AI</span>
+      <span className="custom-text">Government AI</span>
+      <span className="custom-text">Generative AI</span>
+    </div>
+  );
 };
 
 export default TextRotator;
