8578f1638d261fd21b1eff4bbce94e7f2861b94c
canhamzacode <abdulmuizzayo6@gmail.com>
2024-06-27 15:46:24 +0200

    fix: removed duplicate of government ai and updated keyframes

--- src/components/TextRotator/TextRotator.tsx
+++ src/components/TextRotator/TextRotator.tsx
@@ -6,7 +6,6 @@ const TextRotator = () => {
       <span className="custom-text">Generative AI</span>
       <span className="custom-text">Automotive AI</span>
       <span className="custom-text">Government AI</span>
-      <span className="custom-text">Government AI</span>
       <span className="custom-text">Generative AI</span>
     </div>
   );
