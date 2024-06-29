d7d781e59138ff253e5ebf61521669f44ce1ee3d
canhamzacode <abdulmuizzayo6@gmail.com>
2024-06-29 22:58:27 +0200

    feat: mobile nav links

---
+++ src/components/data.ts
@@ -0,0 +1,40 @@
+export const navLinks = [
+  {
+    title: 'Products',
+    items: [
+      { name: 'Scale Data Engine' },
+      { name: 'For Generative AI' },
+      { name: 'For Government' },
+      { name: 'For Automotive' },
+      { name: 'Scale Donovan' },
+      { name: 'Scale GenAI Platform' }
+    ]
+  },
+  {
+    title: 'Government',
+    items: [{ name: 'Defense' }, { name: 'Public Sector' }, { name: 'Federal' }]
+  },
+  {
+    title: 'Resources',
+    items: [
+      { name: 'About' },
+      { name: 'Guides' },
+      { name: 'Careers' },
+      { name: 'AI Readiness Report 2024' },
+      { name: 'Blog' },
+      { name: 'Events' },
+      { name: 'Documentation' }
+    ]
+  },
+  {
+    title: 'Customers',
+    items: [
+      { name: 'Open AI' },
+      { name: 'Toyota' },
+      { name: 'Flexport' },
+      { name: 'Microsoft' },
+      { name: 'Brex' },
+      { name: 'OpenSea' }
+    ]
+  }
+];
